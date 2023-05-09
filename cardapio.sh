c449932b2e0456568051c50afe3bee0ed205132d
Fábio Naconeczny da Silva <113272497+FabioKekw@users.noreply.github.com>
2023-05-09 02:20:03 -0300

    Update cardapio.sh

--- cardapio.sh
+++ cardapio.sh
@@ -11,62 +11,49 @@ BACKTITLE="UNIVERSIDADE FEDERAL DO PARANÁ - CURITIBA"
 TITLE="Cardápio RU's"
 MENU="Escolha seu restaurante universitário:"
 
-OPTIONS=(1 "RU Central"
-         2 "RU Centro Politécnico"
-         3 "RU Jardim Botânico"
- 	 4 "RU Agrárias")
+OPTIONS=(
+    1 "RU Central"
+    2 "RU Centro Politécnico"
+    3 "RU Jardim Botânico"
+    4 "RU Agrárias"
+)
 
 CHOICE=$(dialog --clear \
                 --backtitle "$BACKTITLE" \
                 --title "$TITLE" \
                 --menu "$MENU" \
-                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "$HEIGHT" "$WIDTH" "$CHOICE_HEIGHT" \
                 "${OPTIONS[@]}" \
                 2>&1 >/dev/tty)
 
+scrap() {
+    RU="$1"
+    FILE=$(lynx --dump "https://pra.ufpr.br/ru/$RU/")
+    CARDAPIO=$(echo "$FILE" | sed -n '/Cardápio RU/,$p' | sed '4d;/SENHOR/d' | sed -n '/VOCÊ/q;p')
+    echo "$CARDAPIO"
+}
+
 case $CHOICE in
-        1)
-            FILE=$(lynx --dump https://pra.ufpr.br/ru/ru-central/)
-
-	    echo -e " "
-
-	    CARDAPIO=$(echo "$FILE" | sed -n '/Cardápio RU/,$p' | sed -n '/SENHOR/q;p')
-	    dialog  --clear\
- 		    --title "" \
- 		    --msgbox "$CARDAPIO" \
- 		    0 0
-            ;;
-        2)
-            FILE=$(lynx --dump https://pra.ufpr.br/ru/ru-centro-politecnico/)
-
-            echo -e " "
-
-	    CARDAPIO=$(echo "$FILE" | sed -n '/Cardápio RU/,$p' | sed -n '/SENHOR/q;p')
-            dialog --clear \
-	    	   --title "" \
-                   --msgbox "$CARDAPIO" \
-                   0 0
-            ;;
-        3)
-            FILE=$(lynx --dump https://pra.ufpr.br/ru/cardapio-ru-jardim-botanico/)
-            echo -e " "
-
-	    CARDAPIO=$(echo "$FILE" | sed -n '/Cardápio RU/,$p' | sed -n '/SENHOR/q;p')
-            dialog --clear \
- 		   --title "" \
- 		   --msgbox "$CARDAPIO" \
-                   0 0
-            ;;
- 	4)
-            FILE=$(lynx --dump https://pra.ufpr.br/ru/cardapio-ru-agrarias/)
-
-            echo -e " "
-
-	    CARDAPIO=$(echo "$FILE" | sed -n '/Cardápio RU/,$p' | sed -n '/SENHOR/q;p')
-            dialog --clear \
- 		   --title "" \
- 		   --msgbox "$CARDAPIO" \
-		   0 0
-	    ;;
+    1)
+        ADDRESS=$(scrap "ru-central")
+        echo -e "\n"
+        dialog --clear --title "" --msgbox "$ADDRESS" 0 0
+        ;;
+    2)
+        ADDRESS=$(scrap "ru-centro-politecnico")
+        echo -e "\n"
+        dialog --clear --title "" --msgbox "$ADDRESS" 0 0
+        ;;
+    3)
+        ADDRESS=$(scrap "cardapio-ru-jardim-botanico")
+        echo -e "\n"
+        dialog --clear --title "" --msgbox "$ADDRESS" 0 0
+        ;;
+    4)
+        ADDRESS=$(scrap "cardapio-ru-agrarias")
+        echo -e "\n"
+        dialog --clear --title "" --msgbox "$ADDRESS" 0 0
+        ;;
 esac
+
 clear
